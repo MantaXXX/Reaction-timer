79660264d26d892692f2e939f8a910dd57e69ab8
Jia <pochialu@gmail.com>
2021-03-10 10:50:09 +0800

    add style to App button

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -23,9 +23,9 @@ export default {
 </script>
 
 <style>
-  .rank {
-    font-size: 1.4em;
-    color: #0faf87,
-    font-weight: bold,
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
 }
 </style>
